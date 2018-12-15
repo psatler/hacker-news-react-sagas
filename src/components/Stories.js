0bb70b04074b800a2e3d627e1fca913f214af254
Pablo Satler <pablosatler@gmail.com>
2018-12-15 16:33:29 -0200

    feat: define components and add vscode ignore

---
+++ src/components/Stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Stories.css'
+
+import Story from './Story'
+
+const Stories = ({ stories }) =>
+    <div className="stories">
+        {(stories || []).map(story => 
+            <Story key={story.objectID} story={story} />
+        )}
+    </div>
+
+export default Stories;
